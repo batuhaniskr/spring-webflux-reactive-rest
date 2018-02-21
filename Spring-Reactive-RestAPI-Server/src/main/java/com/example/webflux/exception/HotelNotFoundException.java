c1e98cc1be81d4f882a4c34f2b4147c7f6b968cc
Batuhan Işıker <batuhan.isiker@metglobal.com>
2018-02-21 14:36:43 +0300

    The directory naming has been changed.

--- Spring-Reactive-RestAPI-Server/src/main/java/com/example/webflux/exception/HotelNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.webflux.exception;
-
-
-public class HotelNotFoundException extends RuntimeException {
-
-    public HotelNotFoundException(String message) {
-        super(message);
-    }
-}
